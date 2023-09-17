baedf9c43a906daeefeab9d9e36151443ca08a48
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2023-09-17 20:04:44 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,24 @@
-resource "aws_instance" "web" {
-  ami           = var.image_id
-  instance_type = var.type_of_instance
-  
-  tags = {
-    Name = "Dev"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
-output "public_ip" {
-  value = aws_instance.web.public_ip
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
