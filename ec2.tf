4ccbbed70bbb99057dc203f86b5d440fe734a039
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2023-09-25 20:03:36 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -22,4 +22,32 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
     Envoronment = "dev"
   }
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+   options {
+      instance_metadata_tags = "enabled"
+   }
+  }
+
+  user_data = <<-EOL
+#!/bin/bash
+# Install docker
+apt-get update
+apt-get install -y cloud-utils apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce
+usermod -aG docker ubuntu
+
+# Install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose 
+sudo docker run -d --restart=unless-stopped -p 80:80 -p 443:443 --privileged rancher/rancher
+  EOL
 }
