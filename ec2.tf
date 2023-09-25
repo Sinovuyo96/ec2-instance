68b626482463edd6fc580d196e938066b93a005b
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2023-09-25 21:25:46 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -23,20 +23,21 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOL
 #!/bin/bash
-# Install docker
-apt-get update
-apt-get install -y cloud-utils apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-apt-get update
-apt-get install -y docker-ce
-usermod -aG docker ubuntu
+#install Docker
+sudo yum update -y 
+sudo amazon-linux-extras install docker -y 
+sudo dockerd
+sudo service docker start
+sudo systemctl start docker
+sudo service docker status 
+sudo groupadd docker
+sudo usermod -a -G docker ec2-user 
+newgrp docker 
+docker — version
 
-# Install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+Install docker-compose
+
+curl -L [https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname](https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname) -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose 
 sudo docker run -d --restart=unless-stopped -p 80:80 -p 443:443 --privileged rancher/rancher
   EOL
