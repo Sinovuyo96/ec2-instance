8f8d5107494a46594e04edc3ba679ac41042e26b
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2023-10-14 15:54:48 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "image_id" {
   type = string
-  default = "ami-05cd35b907b4ffe77"
+  default = "ami-0d1cd67c26f5fca19"
   description = "this is my image id"
 }
 
 variable "type_of_instance" {
   type = string
-  default = "t2.micro"
+  default = "t2.medium"
   description = "size of the instance"
 }
